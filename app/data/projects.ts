c976bc1db25d8f78861adde5b438bb3f714994be
Derara <114152134+Deraraf@users.noreply.github.com>
2025-09-15 10:00:10 +0300

    flip the github projects links

--- app/data/projects.ts
+++ app/data/projects.ts
@@ -9,17 +9,15 @@ const projects = [
   },
   {
     title: "E-Commerce Website",
-    description:
-      " building and deploying a fullstack multi user blog platform",
-    tech: ["Next", "TailwindCSS", "Socket.io", "Postgresql"],
+    description: "A responsive e-commerce website built with React, Redux, Express, TailwindCSS, and MongoDB.",
+    tech: ["React", "TailwindCSS", "Redux", "Express", "MongoDB"],
     github: "https://github.com/Deraraf/e-commerceapp",
     demo: "https://e-commerce-app-frontend-orcin.vercel.app",
   },
-  {
-    title: "Multi User Blog Platform",
-    description:
-      "A responsive e-commerce website built with React, Redux, Express, TailwindCSS, and MongoDB.",
-    tech: ["React", "TailwindCSS", "Redux", "Express", "MongoDB"],
+    {
+    title: " Multi User Blog Platform",
+    description: "building and deploying a fullstack multi user blog platform",
+    tech: ["Next", "TailwindCSS", "Socket.io", "Postgresql"],
     github: "https://github.com/Deraraf/webdevblog",
     demo: "https://webdevblog-1.onrender.com",
   },
